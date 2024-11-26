95278c47a749a081362e673a8a2d8e700cba4881
Stark Ernő <stark.erno@gmail.com>
2024-11-26 19:18:31 +0100

    Kész

---
+++ src/app/num/num.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-num',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './num.component.html',
+  styleUrl: './num.component.css'
+})
+export class NumComponent {
+num !: number;
+triple !: number;
+displayTriplaNum: boolean = false;
+resultList: number[] = [];
+
+getNum() {
+  console.log("Triplázás...");
+  const result = this.num * 3;
+  this.resultList.push(result)
+  this.displayTriplaNum = true;
+  console.log(this.resultList);
+  
+}
+}
